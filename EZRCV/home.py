4d41a1ce9e469ff42eef764f2b493d14f046afa9
Alex Gomez <alxgomez123@gmail.com>
2024-02-01 18:58:31 -0800

    created results algorithm and connecting results template

--- EZRCV/home.py
+++ EZRCV/home.py
@@ -4,6 +4,8 @@ from flask import (
 from EZRCV import db
 import sqlalchemy as sa
 from EZRCV.models import Ballot, Entry, Voter
+from collections import deque
+import itertools
 
 bp = Blueprint('home', __name__)
 
@@ -14,7 +16,7 @@ def index():
         if 'vote' in request.form:
             return redirect(url_for('home.vote', ballot_id=request.form['voteCode']))
         elif 'results' in request.form:
-            pass
+            return redirect(url_for('home.results', ballot_id=request.form['resultsCode']))
 
     return render_template('index.html')
 
@@ -46,6 +48,58 @@ def vote(ballot_id):
         db.session.add(Voter(ballot_id=ballot_id, vote=rankings))
         db.session.commit()
 
+        return render_template('index.html')
+
     entries = db.session.execute(sa.select(Entry).where(Entry.ballot_id == ballot_id))
     ballot_info = db.session.scalar(sa.select(Ballot).where(Ballot.id == ballot_id))
+
     return render_template('vote.html', entries=entries, ballot=ballot_info)
+
+
+@bp.route('/<int:ballot_id>/results')
+def results(ballot_id):
+    """Returns a winner to the results template using the instant-runoff voting method."""
+    # rankings(votes) are stored as a list of deques with most preferred candidates on top
+    voters = db.session.scalars(sa.select(Voter).where(Voter.ballot_id == ballot_id))
+    rankings = [deque(int(entry) for entry in reversed(voter.vote.split(" "))) for voter in voters]
+    win_threshold = len(rankings) / 2
+    print(rankings)
+    # candidates points are stored as a dictionary of id keys and vote deque array values
+    # points are determined by the length of these arrays
+    cands = db.session.scalars(sa.select(Entry).where(Entry.ballot_id == ballot_id))
+    cand_pts = {cand.id: [] for cand in cands}
+    for ranking in rankings:
+        cand_pts[ranking.pop()].append(ranking)
+
+    while True:
+        cand_results = list(cand_pts.items())
+        cand_results.sort(key=lambda tup: len(tup[1]), reverse=True)
+
+        if len(cand_results[0][1]) > win_threshold:
+            # the top candidate got more than 50% of the vote
+            result = db.session.scalar(sa.select(Entry.name).where(Entry.id == cand_results[0][0]))
+            break
+        else:
+            elim_threshold = len(cand_results[-1][1])
+            elim_count = 0
+
+            for cand in reversed(cand_results):
+                if len(cand[1]) > elim_threshold:
+                    break
+                elim_count += 1
+
+            if elim_count == len(cand_results):
+                # the remaining candidates have the same number of votes
+                print('its a tie')
+                result_list = db.session.scalars(
+                    sa.select(Entry.name).where(Entry.id.in_(
+                        itertools.islice(cand_results, len(cand_results) - elim_count, len(cand_results)))))
+                result = ", ".join(result_list)
+                break
+            else:
+                for cand in itertools.islice(cand_results, len(cand_results) - elim_count, len(cand_results)):
+                    for ranking in cand_pts.pop(cand[0]):
+                        if ranking[-1] in cand_pts:
+                            cand_pts[ranking.pop()].append(ranking)
+
+    return render_template('results.html', result=result)
